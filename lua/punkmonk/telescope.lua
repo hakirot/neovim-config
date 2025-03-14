76afc0362b60786a3da46446846c02f4f8a5f434
hakirot <hackirot@proton.me>
2025-03-14 01:50:16 -0700

    snippets and old_files

--- lua/punkmonk/telescope.lua
+++ lua/punkmonk/telescope.lua
@@ -2,6 +2,7 @@
 -- Still not sourcing this when in after/plugin -_-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+vim.keymap.set('n', '<leader>r', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>g', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
