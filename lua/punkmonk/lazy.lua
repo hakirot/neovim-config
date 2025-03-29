701dba9b2a298a4311491b94df81f60e54a85bd6
hakirot <hackirot@proton.me>
2025-03-29 16:06:09 -0700

    comment lspsaga because I cant remember which plugin required it and seems unnecessary

--- lua/punkmonk/lazy.lua
+++ lua/punkmonk/lazy.lua
@@ -145,16 +145,16 @@ require("lazy").setup({
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-look",
   },
-  {
-   'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({})
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons',     -- optional
-    }
-  },
+--{
+-- 'nvimdev/lspsaga.nvim',
+--  config = function()
+--      require('lspsaga').setup({})
+--  end,
+--  dependencies = {
+--    'nvim-treesitter/nvim-treesitter', -- optional
+--    'nvim-tree/nvim-web-devicons',     -- optional
+--  }
+--},
   {
     "folke/noice.nvim",
 --  opts = {
