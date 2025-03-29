1e1990c8c1e324f2f6d03905f9db1ce5d7ad4c0d
hakirot <hackirot@proton.me>
2025-03-29 04:04:16 -0700

    experiementing ..

--- lua/punkmonk/lsp_config.lua
+++ lua/punkmonk/lsp_config.lua
@@ -9,8 +9,8 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover , {})
---vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float , { noremap = true, silent = true })
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '<leader>x', vim.diagnostic.open_float, {})
 end
 
 -- FOR REFERENCE
@@ -49,7 +49,7 @@ rt.setup({
 
 
 vim.diagnostic.config({
-  virtual_text = false,
+  virtual_text = true,
   update_in_insert = false,
   underline = true,
   severity_sort = false,
