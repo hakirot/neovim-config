c812c193213a3152de97129cf510b32501cb74f4
hakirot <hackirot@proton.me>
2025-06-26 22:09:38 -0700

    commennt second K keymap override

--- after/plugin/rust.lua
+++ after/plugin/rust.lua
@@ -8,11 +8,11 @@ local bufnr = vim.api.nvim_get_current_buf()
   vim.keymap.set('n', '<Space>k', vim.diagnostic.open_float, {}),
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 )
-vim.keymap.set(
-  "n",
-  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-  function()
-    vim.cmd.RustLsp({'hover', 'actions'})
-  end,
-  { silent = true, buffer = bufnr }
-)
+--vim.keymap.set(
+--  "n",
+--  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+--  function()
+--    vim.cmd.RustLsp({'hover', 'actions'})
+--  end,
+--  { silent = true, buffer = bufnr }
+--)
